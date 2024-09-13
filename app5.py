1d3c4495b11320d6b636266801fe436942cd6552
Ozan Effendi <ozaneffendi018@gmail.com>
2024-09-13 22:04:49 +0500

    Add files via upload

---
+++ app5.py
@@ -0,0 +1,3 @@
+weight = input("Enter your weight:")
+kilo = 0.45 * int(weight)
+print(kilo) 
---
+++ app6.py
@@ -0,0 +1,17 @@
+course = "Python's Course For Beginners"
+# First letter is [0] Zero.
+print(course)
+email = '''
+Hello
+My name is Ozan
+
+I am writing this to you
+
+ThankYou
+'''
+print(email)
+print(course[0])
+print(course[0:])
+print(course[0:3])
+another = course[:]
+print(another)
