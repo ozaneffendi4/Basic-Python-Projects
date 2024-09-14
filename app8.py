a4dc96c55ca9ba8dd75e4187a311b5659377a73d
Ozan Effendi <ozaneffendi018@gmail.com>
2024-09-14 15:27:12 +0500

    Add files via upload

---
+++ app8.py
@@ -0,0 +1,8 @@
+course = 'Python for beginners'
+print(len(course))
+print(course.upper())
+print(course)
+print(course.lower())
+print(course.find('n'))
+print(course.replace('beginners' , 'Absolute beginners'))
+print('Python' in course)
