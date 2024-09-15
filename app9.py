81f617f1751eb8f0ad9146e6ad890b20587814e8
Ozan Effendi <ozaneffendi018@gmail.com>
2024-09-15 12:27:12 +0500

    Add files via upload

---
+++ app9.py
@@ -0,0 +1,17 @@
+print(10 / 3) #Division
+print(10 % 3) # Remainder
+print(10 ** 3) # To the power
+print(10 + 3) #Addition
+print(10 - 3) #Subtraction 
+print(10 * 3) #Multiplication
+
+x = 10
+x = x + 3
+print(x)
+
+y = 20
+
+print(x + y)
+
+z = 10 + 3 * 2
+print(z) # Answer will be '16' Basic Math (BODMAS)
